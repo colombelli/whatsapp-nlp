355f40bad2e70c314b379c31caafa6281a941bd9
colombelli <xcolombelli@gmail.com>
2021-04-12 14:27:02 -0300

    Add methods for generating vocabulary related objects
    Used in the training process of the LSTM.

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,7 +1,7 @@
 import re
 import pandas as pd
 import numpy as np
-
+import random
 
 class DataProcessing:
 
@@ -9,7 +9,7 @@ class DataProcessing:
     Data processing for whatsapp imported messages.
     It operates aiming to converte all messages into a single string containing the contact sender
     name followed by a ':' and the message. Everything is separated by a ' ' which is used as word
-    delimiter separator.
+    delimiter separator to build the vocabulary.
 
     Args:
         file_path (str): path containing the whatsapp imported messages file
@@ -20,8 +20,16 @@ class DataProcessing:
         self.history = None
         self.all_messages = None
 
+        self.vocabulary = None
+        self.word2idx = None
+        self.idx2word = None
+        self.vector_data_idx = []
+        self.vector_data_word = None
+
         self.__read_history()
         self.__build_all_messages_string()
+        self.__generate_vocabulary()
+        self.__vectorize_words()
 
 
     def __read_history(self):
@@ -51,10 +59,35 @@ class DataProcessing:
         history = self.history.loc[:, ['name', 'msg']]
 
         all_messages = ""
-        for index, row in history.iterrows():
+        for _, row in history.iterrows():
             name = row['name']
             msg = row['msg']
             all_messages += name + ": " + msg + " "
         
+
+        # Eliminates <PersonA omitted> messages
+        names = list(self.history['name'].unique())
+        for name in names:
+            all_messages = all_messages.replace("<"+name+" omitted>", '')
+
+        # Eliminates 'This message was deleted' messages
+        all_messages = all_messages.replace("This message was deleted", '')
+
         self.all_messages = all_messages
         return
+
+
+    def __generate_vocabulary(self):
+
+        self.vector_data_word = self.all_messages.split(" ")
+        self.vocabulary = sorted(set(self.vector_data_word))
+        self.word2idx = {u:i for i, u in enumerate(self.vocabulary)}
+        self.idx2word = np.array(self.vocabulary)
+        return
+
+
+    def __vectorize_words(self):
+        for word in self.vector_data_word:
+            self.vector_data_idx.append(self.word2idx[word])
+        return
+        
